c298ce7dc838d000871218d71822fbcf22a41d5c
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-10-23 08:45:29 +0000

    Fixed path

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ FREESHOW="~/FreeShow-1.2.9-x86_64.AppImage"
 # Start freeshow in wayland mode
 hyprctl dispatch exec "$FREESHOW --ozone-platform=wayland"
 
-hyprctl hyprpaper preload "~/SU/SU Wallpaper.png"
-hyprctl hyprpaper wallpaper ",~/SU/SU Wallpaper.png"
+hyprctl hyprpaper preload "~/repos/su-setup/SU Wallpaper.png"
+hyprctl hyprpaper wallpaper ",~/repos/su-setup/SU Wallpaper.png"
